f73ecc4bdd681a6246930ab16d3012a85ca355fd
Assaf Berg <asssaf@users.noreply.github.com>
2025-10-19 09:35:39 -0700

    ensure directories exist before mounting them

--- run.sh
+++ run.sh
@@ -5,6 +5,9 @@ set -eu
 # usage:
 # run.sh --prompt "what is the weather in SF?"
 
+mkdir -p "${HOME}/.gemini"
+mkdir -p "${HOME}/.config/configstore/update-notifier-\@google"
+
 IMAGE=asssaf/gemini-cli
 docker run --rm -it \
 	-v ${HOME}/.config/configstore/update-notifier-@google:/home/user/.config/configstore/update-notifier-@google \
